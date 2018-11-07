7d5b0c01ba103bc48fff98e35f0b4838481c122d
rene.meijer@gmail.com <rene.meijer@gmail.com>
2018-11-07 12:29:31 +0100

    eulapge

---
+++ CocktailApp/CocktailApp/Views/EulaPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CocktailApp.Data;
+using CocktailApp.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CocktailApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EulaPage : ContentPage
+    {
+        public MainPage HomePage { get; set; }
+
+        public EulaPage()
+        {
+            InitializeComponent();
+            HomePage = new MainPage();
+
+            BindingContext = this;
+        }
+
+        private async void Accept_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new LoginPage());
+        }
+
+
+        private async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new RegisterPage());
+        }
+        
+    }
+}
